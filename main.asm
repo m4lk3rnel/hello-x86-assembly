e9b3e9d45fe4f3a5c42cd964dc2f2051434ae56f
m4l <82606401+m4lk3rnel@users.noreply.github.com>
2025-07-20 13:17:34 +0300

    formatting issues

--- main.asm
+++ main.asm
@@ -1,31 +1,32 @@
-									  ;this is a program for 32-bit Linux systems. 
-section		.data					  ;create the data section
+					  ;this is a program for 32-bit Linux systems. 
 
-	msg     db "Hello world!", 0xa    ;declare the message, 0xa = \n in ASCII
+section	.data		                  ;create the data section
 
-	len		equ $ - msg               ;$ - the location (memory address) at the end of the string
+        msg     db "Hello world!", 0xa    ;declare the message, 0xa = \n in ASCII
 
+        len     equ $ - msg               ;$ - the location (memory address) at the end of the string
 	                                  ;msg - the location (memory address) at the beginning of the string
-section		.text					  ;create the text section
-	global _start				      ;tells the linker where the entry point of the program is.
 
-_start:								  ;entry point for the program.
+section	.text			          ;create the text section
+        global _start		          ;tells the linker where the entry point of the program is.
 
-	mov eax, 4						  ;invoke sys_write by moving the number 4 into the EAX register.
-									  ;the number 4 is the "id" of the syscall.
+_start:					  ;entry point for the program.
 
-	mov edx, len					  ;move the memory address of the length of the message
-									  ;to the EDX register
-									  ;edx - general-purpose register. can be used for data
-									  ;and math
+        mov eax, 4			  ;invoke sys_write by moving the number 4 into the EAX register.
+					  ;the number 4 is the "id" of the syscall.
 
-	mov ecx, msg					  ;move the the memory address of msg to the ECX register.
-									  ;ecx - also a general-purpose register.
+        mov edx, len			  ;move the memory address of the length of the message
+					  ;to the EDX register
+					  ;edx - general-purpose register. can be used for data
+					  ;and math
 
-	mov ebx, 1						  ;move 1 (file descriptor for STDOUT) to the EBX register.
-	int 0x80						  ;user mode -> KERNEL mode so the syscall is handled by the kernel.
+        mov ecx, msg			  ;move the the memory address of msg to the ECX register.
+                                          ;ecx - also a general-purpose register.
 
-	mov eax, 1						  ;1 - sys_exit
-	int 0x80
+        mov ebx, 1		          ;move 1 (file descriptor for STDOUT) to the EBX register.
+        int 0x80	                  ;user mode -> KERNEL mode so the syscall is handled by the kernel.
+
+        mov eax, 1			  ;1 - sys_exit
+        int 0x80
 
 	
